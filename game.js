a8d2d09f0782dd2411e427a0361800caca6b4015
shatVM <90845743+shatVM@users.noreply.github.com>
2024-02-05 15:43:58 +0200

    1

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
+//https://phaser.io/tutorials/making-your-first-phaser-3-game/part1
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -9,27 +17,196 @@ var config = {
     }
 };
 
+//Змінні
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var scoreText;
+var gameOver = false;
+
+
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 
+    this.load.spritesheet('dude',
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
 }
 
-function create ()
-{
+
+
+function create() {
+
+    //Додаємо небо
     this.add.image(400, 300, 'sky');
-    //this.add.image(400, 300, 'star');
-    //this.add.image(100, 100, 'star');
+
+    //Додаємо платформи
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    //Створюємо гравця
+    player = this.physics.add.sprite(350, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    //Колізія гравця та платформ
+    this.physics.add.collider(player, platforms);
+
+    //Обробка натискання клавіш
+    cursors = this.input.keyboard.createCursorKeys();
+
+    //Додавання зірок
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 20,
+        setXY: { x: 12, y: 0, stepX: 40 }
+    });
+
+    stars.children.iterate(function (child) {
+
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+
+    });
+
+    //Текстовий напис для рахунку
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    //Колізія гравця та платформ
+    this.physics.add.collider(player, platforms);
+
+    //Колізія зірок та платформ
+    this.physics.add.collider(stars, platforms);
+
+    //Колізія гравця та зірок
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    //Генерація бомб 
+
+    bombs = this.physics.add.group({
+        key: 'bomb',
+        repeat: 5,
+        setXY: { x: 25, y: 0, stepX: 120 }
+    });
+
+    bombs.children.iterate(function (child) {
+
+        //child.setBounceY(Phaser.Math.FloatBetween(.2, 12));
+        //child.setVelocityY(Phaser.Math.FloatBetween(-500, 500));
+        child.setBounce(Phaser.Math.FloatBetween(0, 1));
+        child.setCollideWorldBounds(true);
+        child.setVelocityX(Phaser.Math.FloatBetween(-500, 500));
+
+
+    });
+    //bombs.setBounce(0.2);
+    //bombs.setCollideWorldBounds(true);
+
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
-function update ()
-{
+function update() {
+
+
+
+    //
+    if (gameOver) {
+        return;
+    }
+
+    //
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    // if (cursors.down.isDown) {
+    //     player.setVelocityY(200);
+    // }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+}
+
+//Відпрацювання колізії гравця та зірок
+function collectStar(player, star) {
+    star.disableBody(true, true);
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    //
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 800) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}
+
+
+//Відпрацювання колізії гравця та бомб
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
